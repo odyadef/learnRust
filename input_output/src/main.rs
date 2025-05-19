c325c6956b8388445608c7e21535f99baba13769
Odya Def <odyadef@gmail.com>
2025-05-19 21:25:53 +0400

    add some input examples

--- input_output/src/main.rs
+++ input_output/src/main.rs
@@ -1,6 +1,13 @@
+use std::io;
+
 fn main() {
-    let name: &str = "Jane";
-    println!("Hello, \n{}!", name);
+    println!("Please, tape your name: ");
+    let mut name = String::new();
+    let res = io::stdin()
+        .read_line(&mut name)
+        .expect("Cannot read string");
+    println!("Hello, {name}!");
+    println!("result = {}", res);
     let x = 10;
     let y = 15;
     println!("{} + {1} = {sum}", x, y, sum = x + y);
--- input_output/target/debug/deps/input-c2695a366af7592d.d
+++ input_output/target/debug/deps/input-c2695a366af7592d.d
@@ -1,5 +1,5 @@
-/home/odyadef/Code/rust/learnRust/input/target/debug/deps/libinput-c2695a366af7592d.rmeta: src/main.rs
+/home/odyadef/Code/rust/learnRust/input_output/target/debug/deps/libinput-c2695a366af7592d.rmeta: src/main.rs
 
-/home/odyadef/Code/rust/learnRust/input/target/debug/deps/input-c2695a366af7592d.d: src/main.rs
+/home/odyadef/Code/rust/learnRust/input_output/target/debug/deps/input-c2695a366af7592d.d: src/main.rs
 
 src/main.rs:
