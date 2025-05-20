7f459ba494c743e779b4b9337a297fb498a7f4b1
Odya Def <odyadef@gmail.com>
2025-05-20 13:27:47 +0400

    add some more input examples

--- input_output/src/main.rs
+++ input_output/src/main.rs
@@ -5,10 +5,23 @@ fn main() {
     let mut name = String::new();
     let res = io::stdin()
         .read_line(&mut name)
-        .expect("Cannot read string");
-    println!("Hello, {name}!");
+        .expect("Cannot read input");
+    println!("Hello, {}!", name.trim());
     println!("result = {}", res);
-    let x = 10;
-    let y = 15;
+
+    println!("Please, tape first number");
+    let mut first_num = String::new();
+    io::stdin()
+        .read_line(&mut first_num)
+        .expect("Cannot read input");
+    let x: i32 = first_num.trim().parse().expect("Please, tape a number");
+    
+    println!("Please, tape second number");
+    let mut second_num = String::new();
+    io::stdin()
+        .read_line(&mut second_num)
+        .expect("Cannot read input");
+    let y: i32 = second_num.trim().parse().expect("Please, tape a number");    
+    
     println!("{} + {1} = {sum}", x, y, sum = x + y);
 }
---
+++ input_output/target/debug/deps/input-c9aea682daa89812.d
@@ -0,0 +1,9 @@
+/home/odyadef/Code/rust/learnRust/input_output/target/debug/deps/libinput-c9aea682daa89812.rmeta: src/main.rs Cargo.toml
+
+/home/odyadef/Code/rust/learnRust/input_output/target/debug/deps/input-c9aea682daa89812.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
