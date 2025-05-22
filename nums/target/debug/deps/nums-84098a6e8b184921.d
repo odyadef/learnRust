c2b6070e11b19518f6f23b29ed5abb320b608c4c
Odya Def <odyadef@gmail.com>
2025-05-22 12:07:39 +0400

    a little change

---
+++ nums/target/debug/deps/nums-84098a6e8b184921.d
@@ -0,0 +1,5 @@
+/home/odyadef/Code/rust/learnRust/nums/target/debug/deps/libnums-84098a6e8b184921.rmeta: src/main.rs
+
+/home/odyadef/Code/rust/learnRust/nums/target/debug/deps/nums-84098a6e8b184921.d: src/main.rs
+
+src/main.rs:
