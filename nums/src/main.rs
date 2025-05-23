3c831357776482a8203a670715f2fb403a196353
Odya Def <odyadef@gmail.com>
2025-05-23 11:42:47 +0400

    add nums examples

--- nums/src/main.rs
+++ nums/src/main.rs
@@ -17,13 +17,16 @@ fn main() {
     let x: i32 = first.trim().parse().expect("Must be an integer");
     let y: i32 = second.trim().parse().expect("Must be an integer");
 
+    let pi = 3.1415_f64;
+
     let sum = x + y;
     let difference = x - y;
     let product = x * y;
     let truncated = x / y;
-    let remainder = -x % y;
+    let remainder = x % y;
+    let xpi = x as f64 * pi;
 
     println!("sum = {sum}\ndifference = {difference}\nproduct = {product}\ntruncated = {truncated}\nremainder = {remainder}");
-
+    println!("{x} * {pi} = {xpi}");
 
 }
