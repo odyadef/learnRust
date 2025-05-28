4fc4b7d0cb5f591f96f80a9ca6946954bdcee25f
Odya Def <odyadef@gmail.com>
2025-05-28 13:55:49 +0400

    add some array examples

--- arrays/src/main.rs
+++ arrays/src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    let arr = [1, 2, 3];
+    let arr1 = [11];
+    let arr2 = [12,];
+
+    let arr3: [u8; 3] = [22, 23, 24];
+    let arr4 = [33_i16, 34, 35];
+
+    let zero_arr = [0; 6];
+
+    println!{"{:?}", arr};
+    println!{"{:?}", arr1};
+    println!{"{:?}", arr2};
+    println!{"{:?}", arr3};
+    println!{"{:?}", arr4};
+    println!{"{zero_arr:?}"};
+
+    let emoji_arr: [char; 6];
+    emoji_arr = ['🦀', '🐈', '🐦', '🐓', '🦫', '🦄'];
+
+    println!("{emoji_arr:?}");
+    println!("{}", emoji_arr[5]);
+
+    let (first, second, third) = (0, 1, 2);
+    println!("{}, {}, {}", emoji_arr[first], emoji_arr[second], emoji_arr[third]);
+
+    let mut c_arr = ['a', 'b'];
+    println!("{c_arr:?}");
+    c_arr[0] = emoji_arr[4];
+    println!("{c_arr:?}");
+
 }
