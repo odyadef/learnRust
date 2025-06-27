968a6f5a8940a71b39f1817c0eaed548be1326fd
Odya Def <odyadef@gmail.com>
2025-06-27 13:05:30 +0400

    add some examples

--- forwhileloop/src/main.rs
+++ forwhileloop/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+
+    for i in 0..3 { // for <loop valuable> in start..end 
+        println!("{}", i)
+    }
+
+    for _ in 1..=3 { // include 5
+        println!("check")
+    }
+
+    let (start, end) = (3, 5);
+    for x in start..=end {
+        println!("#{x}");
+    }
+
+    for i in -3..=0 {
+        println!("{i}");
+    }
 }
