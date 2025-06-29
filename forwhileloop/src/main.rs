6d8174dde3f5cecd96650287eb66c72afd206ff2
Odya Def <odyadef@gmail.com>
2025-06-29 21:36:13 +0400

    examples for

--- forwhileloop/src/main.rs
+++ forwhileloop/src/main.rs
@@ -1,10 +1,12 @@
+use std::io;
+
 fn main() {
 
     for i in 0..3 { // for <loop valuable> in start..end 
         println!("{}", i)
     }
 
-    for _ in 1..=3 { // include 5
+    for _ in 1..=3 { // include 3
         println!("check")
     }
 
@@ -15,5 +17,18 @@ fn main() {
 
     for i in -3..=0 {
         println!("{i}");
+    }
+
+    let mut input = String::new();
+    for _ in 0..3 {
+        println!("Please type a number");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Cannot read input");
+        let num: i32 = input.trim()
+            .parse().expect("Must be a number");
+        println!("Number #{num}");
+
+        input.clear();
     }
 }
